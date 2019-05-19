b73a3e2b77a4b5ee99bf8846ed2b604518ebaef5
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-05-19 13:58:32 +0530

    Create set21.c

---
+++ set21.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[20];
+    scanf("%s",str);
+    if(str[0]=='s')
+    {
+        printf("yes");
+    }
+else 
+{
+    printf("no");
+}
+return 0;
+}
